96a6be013fe2b6d63dc631932fe371518e036c81
Randolph Amaya <ra@Mac.lan>
2025-06-05 15:36:35 -0400

    add ss3 access rules

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -1,5 +1,14 @@
 import { defineStorage } from '@aws-amplify/backend';
 
+
+
+
 export const storage = defineStorage({
-  name: 'personal-panacea-s3-bucket',
-});
+    name: 'personal-panacea-s3-bucket',
+    access: (allow) => ({
+      'public/*': [
+        allow.guest.to(['read', 'write', 'delete']),
+        allow.authenticated.to(['read', 'write', 'delete']), 
+      ]
+    })
+  });
